e4d080f4693ac89ccea114975c3ad7f3b245e58f
Mille <mille.bostrom@testura.net>
2017-01-19 21:00:24 +0100

    Added test for paranthesized lambda argument

---
+++ src/Testura.Code.Tests/Generators/Common/Arguments/ArgumentTypes/ParenthesizedLambdaArgumentTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NUnit.Framework;
+using Testura.Code.Generators.Common.Arguments.ArgumentTypes;
+using Testura.Code.Models;
+using Testura.Code.Statements;
+
+namespace Testura.Code.Tests.Generators.Common.Arguments.ArgumentTypes
+{
+    [TestFixture]
+    public class ParenthesizedLambdaArgumentTests
+    {
+        [Test]
+        public void GetArgumentSyntax_WhenCreatingEmpty_ShouldGetCorrectCode()
+        {
+            var argument = new ParenthesizedLambdaArgument(Statement.Expression.Invoke("MyMethod").AsExpression());
+            var syntax = argument.GetArgumentSyntax();
+            
+            Assert.IsInstanceOf<ArgumentSyntax>(syntax);
+            Assert.AreEqual("()=>MyMethod()", syntax.ToString());
+        }
+    }
+}
