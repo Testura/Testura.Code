2a8fec265d332d9f2ecd3901a29689527874d7a7
Mille <mille.bostrom@testura.net>
2017-02-04 09:56:34 +0100

    Updated with comments in the compiler class

--- src/Testura.Code/Compilations/Compiler.cs
+++ src/Testura.Code/Compilations/Compiler.cs
@@ -32,16 +32,32 @@ namespace Testura.Code.Compilations
             };
         }
 
+        /// <summary>
+        /// Compile code from a file without creating a dll
+        /// </summary>
+        /// <param name="pathsToCsFiles">Paths to cs files</param>
+        /// <returns>The result from the compilation</returns>
         public async Task<CompileResult> CompileFileInMemoryAsync(params string[] pathsToCsFiles)
         {
             return await CompileFilesAsync(null, pathsToCsFiles);
         }
 
+        /// <summary>
+        /// Compile code from a string source without creating a dll
+        /// </summary>
+        /// <param name="source">Source strings to compile</param>
+        /// <returns>The result from the compilation</returns>
         public async Task<CompileResult> CompileSourceInMemoryAsync(params string[] source)
         {
             return await CompileSourceAsync(null, source);
         }
 
+        /// <summary>
+        /// Compile code from a file into a dll
+        /// </summary>
+        /// <param name="outputPath">Where we should save the dlls</param>
+        /// <param name="pathsToCsFiles">Path to the cs files</param>
+        /// <returns>The result from the compilation</returns>
         public async Task<CompileResult> CompileFilesAsync(string outputPath, params string[] pathsToCsFiles)
         {
             if (outputPath == null)
@@ -63,6 +79,12 @@ namespace Testura.Code.Compilations
             return await CompileSourceAsync(outputPath, source);
         }
 
+        /// <summary>
+        /// Compile code from a string source into a dll
+        /// </summary>
+        /// <param name="outputPath">Where we should save the dlls</param>
+        /// <param name="source">Source string to compile</param>
+        /// <returns>The result of the compilation</returns>
         public async Task<CompileResult> CompileSourceAsync(string outputPath, params string[] source)
         {
             if (source.Length == 0)
