1908dc55d7a3331e16e2c4631e40aadfbd3e609a
MilleBo <mille.bostrom@testura.net>
2017-03-08 20:10:45 +0100

    - New class for simple lambda arguments (#16)
    - ParenthesizedLambdaArgument now also accept blocksyntax

--- src/Testura.Code.Tests/Generators/Common/Arguments/ArgumentTypes/ParenthesizedLambdaArgumentTests.cs
+++ src/Testura.Code.Tests/Generators/Common/Arguments/ArgumentTypes/ParenthesizedLambdaArgumentTests.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
 using NUnit.Framework;
+using Testura.Code.Generators.Common;
 using Testura.Code.Generators.Common.Arguments.ArgumentTypes;
+using Testura.Code.Models;
 using Testura.Code.Statements;
 
 namespace Testura.Code.Tests.Generators.Common.Arguments.ArgumentTypes
@@ -16,6 +18,28 @@ namespace Testura.Code.Tests.Generators.Common.Arguments.ArgumentTypes
             
             Assert.IsInstanceOf<ArgumentSyntax>(syntax);
             Assert.AreEqual("()=>MyMethod()", syntax.ToString());
+        }
+
+        [Test]
+        public void GetArgumentSyntax_WhenCreatingWithParameter_ShouldGetCorrectCode()
+        {
+            var argument = new ParenthesizedLambdaArgument(Statement.Expression.Invoke("MyMethod").AsExpression(), new[] { new Parameter("myPara", typeof(int)) });
+            var syntax = argument.GetArgumentSyntax();
+
+            Assert.IsInstanceOf<ArgumentSyntax>(syntax);
+            Assert.AreEqual("(myPara)=>MyMethod()", syntax.ToString());
+        }
+
+        [Test]
+        public void GetArgumentSyntax_WhenCreatingWithWithBlock_ShouldGetCorrectCode()
+        {
+            var block = BodyGenerator.Create(Statement.Expression.Invoke("MyMethod").AsStatement());
+
+            var argument = new ParenthesizedLambdaArgument(block, new[] { new Parameter("myPara", typeof(int)) });
+            var syntax = argument.GetArgumentSyntax();
+
+            Assert.IsInstanceOf<ArgumentSyntax>(syntax);
+            Assert.AreEqual("(myPara)=>{MyMethod();}", syntax.ToString());
         }
     }
 }
