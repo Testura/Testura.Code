e4d99f6f75178cf5ae08c005144500bdaa99edf5
mille.bostrom@outlook.com <mille.bostrom@outlook.com>
2022-01-17 18:57:06 +0100

    Added interfaceBuildMember

---
+++ src/Testura.Code/Builders/BuildMembers/InterfaceBuildMember.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Testura.Code.Builders.BuildMembers;
+
+public class InterfaceBuildMember : IBuildMember
+{
+    private readonly TypeDeclarationSyntax _interface;
+
+    public InterfaceBuildMember(InterfaceBuilder interfaceBuilder)
+    {
+        _interface = interfaceBuilder.BuildWithoutNamespace();
+    }
+
+    public InterfaceBuildMember(TypeDeclarationSyntax @interface)
+    {
+        _interface = @interface;
+    }
+
+    public SyntaxList<MemberDeclarationSyntax> AddMember(SyntaxList<MemberDeclarationSyntax> members)
+    {
+        return members.Add(_interface);
+    }
+}
